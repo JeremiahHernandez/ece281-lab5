45bc4bc6412c975f4fe7653773c1a1bd0fa1a6f4
JeremiahHernandez <C28jeremiah.hernandez@afacademy.af.edu>
2026-05-07 23:02:21 -0600

    maybefinal?

--- src/controller_fsm.vhd
+++ src/controller_fsm.vhd
@@ -22,24 +22,43 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity controller_fsm is
-    Port ( i_reset : in STD_LOGIC;
-           i_adv : in STD_LOGIC;
+    Port ( i_clk   : in  STD_LOGIC;
+           i_reset : in  STD_LOGIC;
+           i_adv   : in  STD_LOGIC;
            o_cycle : out STD_LOGIC_VECTOR (3 downto 0));
 end controller_fsm;
 
 architecture FSM of controller_fsm is
 
-begin
+    type state_type is (s_reset, s_op1, s_op2, s_result);
+    signal r_state : state_type := s_reset;
 
+begin
+    process(i_clk, i_reset)
+    begin
+        if (i_reset = '1') then
+            r_state <= s_reset;
+        elsif (rising_edge(i_clk)) then
+            if (i_adv = '1') then
+                case r_state is
+                    when s_reset =>
+                        r_state <= s_op1;
+                    when s_op1 =>
+                        r_state <= s_op2;
+                    when s_op2 =>
+                        r_state <= s_result;
+                    when s_result =>
+                        r_state <= s_reset;
+                end case;
+            end if;
+        end if;
+    end process;
+
+    o_cycle <= "0001" when r_state = s_reset else
+               "0010" when r_state = s_op1   else
+               "0100" when r_state = s_op2   else
+               "1000" when r_state = s_result else
+               "0001";
 
 end FSM;
--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -32,6 +32,7 @@ entity top_basys3 is
         sw      :   in std_logic_vector(7 downto 0); -- operands and opcode
         btnU    :   in std_logic; -- reset
         btnC    :   in std_logic; -- fsm cycle
+        btnL    :   in std_logic;
         
         -- outputs
         led :   out std_logic_vector(15 downto 0);
@@ -43,17 +44,135 @@ entity top_basys3 is
 end top_basys3;
 
 architecture top_basys3_arch of top_basys3 is 
-  
-	-- declare components and signals
 
-  
+    -- Component Declarations
+    component controller_fsm is
+        port (
+            i_clk   : in  std_logic;
+            i_reset : in  std_logic;
+            i_adv   : in  std_logic;
+            o_cycle : out std_logic_vector(3 downto 0)
+        );
+    end component;
+
+    component ALU is
+        port (
+            i_A      : in  std_logic_vector(7 downto 0);
+            i_B      : in  std_logic_vector(7 downto 0);
+            i_op     : in  std_logic_vector(2 downto 0);
+            o_result : out std_logic_vector(7 downto 0);
+            o_flags  : out std_logic_vector(3 downto 0)
+        );
+    end component;
+
+    component twos_comp is
+        port (
+            i_bin  : in  std_logic_vector(7 downto 0);
+            o_sign : out std_logic;
+            o_hund : out std_logic_vector(3 downto 0);
+            o_tens : out std_logic_vector(3 downto 0);
+            o_ones : out std_logic_vector(3 downto 0)
+        );
+    end component;
+
+    component TDM4 is
+        generic ( k_WIDTH : natural  := 4 );
+        port ( i_clk        : in  std_logic;
+               i_reset      : in  std_logic;
+               i_D3         : in  std_logic_vector (k_WIDTH - 1 downto 0);
+               i_D2         : in  std_logic_vector (k_WIDTH - 1 downto 0);
+               i_D1         : in  std_logic_vector (k_WIDTH - 1 downto 0);
+               i_D0         : in  std_logic_vector (k_WIDTH - 1 downto 0);
+               o_data       : out std_logic_vector (k_WIDTH - 1 downto 0);
+               o_sel        : out std_logic_vector (3 downto 0)
+        );
+    end component;
+
+    component sevenseg_decoder is
+        port ( i_Hex   : in  std_logic_vector (3 downto 0);
+               o_seg_n : out std_logic_vector (6 downto 0));
+    end component;
+
+    component clock_divider is
+        generic ( k_DIV : natural := 2 );
+        port ( i_clk   : in  std_logic;
+               i_reset : in  std_logic;
+               o_clk   : out std_logic
+        );
+    end component;
+
+    -- Signals
+    signal w_clk_tdm : std_logic;
+    signal w_cycle   : std_logic_vector(3 downto 0);
+    signal w_op1, w_op2, w_alu_res, w_mux_out : std_logic_vector(7 downto 0);
+    signal w_flags   : std_logic_vector(3 downto 0);
+    signal w_sign, w_hund, w_tens, w_ones, w_dec_in : std_logic_vector(3 downto 0);
+    signal w_sel_an  : std_logic_vector(3 downto 0);
+    signal w_sign_bit : std_logic;
+
 begin
-	-- PORT MAPS ----------------------------------------
 
-	
-	
-	-- CONCURRENT STATEMENTS ----------------------------
-	
-	
-	
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            if btnU = '1' then
+                w_op1 <= (others => '0');
+                w_op2 <= (others => '0');
+            elsif w_cycle = "0010" then
+                w_op1 <= sw;
+            elsif w_cycle = "0100" then
+                w_op2 <= sw;
+            end if;
+        end if;
+    end process;
+
+    w_mux_out <= w_op1     when w_cycle = "0010" else
+                 w_op2     when w_cycle = "0100" else
+                 w_alu_res when w_cycle = "1000" else
+                 x"00";
+
+    -- Port Maps
+    u_fsm : controller_fsm port map (
+        i_clk   => clk,
+        i_reset => btnU,
+        i_adv   => btnC,
+        o_cycle => w_cycle
+    );
+
+    u_alu : ALU port map (
+        i_A      => w_op1,
+        i_B      => w_op2,
+        i_op     => sw(2 downto 0),
+        o_result => w_alu_res,
+        o_flags  => w_flags
+    );
+
+    u_twos : twos_comp port map (
+    i_bin  => w_mux_out,
+    o_sign => w_sign_bit,
+    o_hund => w_hund,
+    o_tens => w_tens,
+    o_ones => w_ones
+);
+
+    u_clkdiv : clock_divider 
+        generic map ( k_DIV => 50000 )
+        port map ( i_clk => clk, i_reset => btnL, o_clk => w_clk_tdm );
+
+    u_tdm : TDM4 port map (
+        i_clk => w_clk_tdm, i_reset => btnU,
+        i_D3 => w_sign, i_D2 => w_hund, i_D1 => w_tens, i_D0 => w_ones,
+        o_data => w_dec_in, o_sel => w_sel_an
+    );
+
+    u_seg : sevenseg_decoder port map (
+        i_Hex => w_dec_in, o_seg_n => seg
+    );
+
+    led(3 downto 0)   <= w_cycle;
+    led(15 downto 12) <= w_flags; 
+    led(11 downto 4)  <= (others => '0');
+
+    an <= "1111" when w_cycle = "0001" else w_sel_an;
+
 end top_basys3_arch;
