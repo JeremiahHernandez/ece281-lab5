2234cb119e68314426cbb2313f8370e6f6ec4463
JeremiahHernandez <C28jeremiah.hernandez@afacademy.af.edu>
2026-05-07 23:16:31 -0600

    test2

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -37,17 +37,23 @@ end ALU;
 architecture Behavioral of ALU is
     signal w_result : std_logic_vector(8 downto 0);
 begin
+
     process(i_A, i_B, i_op)
     begin
         case i_op is
             when "000" => -- ADD
                 w_result <= std_logic_vector(resize(unsigned(i_A), 9) + resize(unsigned(i_B), 9));
-            when "001" => -- SUB
+            
+            when "001" => -- SUB (A - B)
+                -- This will naturally set w_result(8) to '1' if A >= B (No Borrow)
                 w_result <= std_logic_vector(resize(unsigned(i_A), 9) - resize(unsigned(i_B), 9));
+            
             when "010" => -- AND
                 w_result <= ('0' & (i_A and i_B));
+            
             when "011" => -- OR
                 w_result <= ('0' & (i_A or i_B));
+                
             when others =>
                 w_result <= (others => '0');
         end case;
@@ -55,13 +61,14 @@ begin
 
     o_result <= w_result(7 downto 0);
 
-    -- Flag Logic (NZCV)
-    o_flags(3) <= w_result(7); -- N
-    o_flags(2) <= '1' when w_result(7 downto 0) = x"00" else '0'; -- Z
-    o_flags(1) <= w_result(8); -- C
+    -- Flag Mapping (NZCV)
+    o_flags(3) <= w_result(7);                                    -- N: Negative
+    o_flags(2) <= '1' when w_result(7 downto 0) = x"00" else '0'; -- Z: Zero
+    o_flags(1) <= w_result(8);                                    -- C: Carry (No Borrow)
     
-    -- V: Signed Overflow logic
+    -- V: Signed Overflow
     o_flags(0) <= ((not i_A(7) and not i_B(7) and w_result(7)) or (i_A(7) and i_B(7) and not w_result(7))) when i_op = "000" else
                   ((not i_A(7) and i_B(7) and w_result(7)) or (i_A(7) and not i_B(7) and not w_result(7))) when i_op = "001" else
                   '0';
+
 end Behavioral;
